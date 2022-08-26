f1ddcc8edb78cd7acf7d2fafef0ed694e9ba8eba
phuong <phuong@wolffun>
2022-08-26 17:05:10 +0700

    add transfer wei func

--- transactions.go
+++ transactions.go
@@ -80,3 +80,64 @@ func (c account) NewSmcTx(ctx context.Context, tx SmcTxData) (*bind.TransactOpts
 
 	return txOps, nil
 }
+
+type TxData struct {
+	To    string
+	Value *big.Int
+}
+
+func (c account) NewTx(ctx context.Context, data TxData) (*bind.TransactOpts, error) {
+	var err error
+
+	pri := c.pri
+	if len(pri) == 0 {
+		pri = c.pri
+	}
+
+	priK, err := crypto.HexToECDSA(c.pri)
+	if err != nil {
+		return nil, err
+	}
+
+	pubK := priK.Public()
+	pubKECDSA, ok := pubK.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, errors.New("public key is not type of *ecdsa.PublicKey")
+	}
+
+	from := crypto.PubkeyToAddress(*pubKECDSA)
+	to := common.HexToAddress(data.To)
+
+	gasPrice, err := c.Client.SuggestGasPrice(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	gasLimit, err := c.Client.EstimateGas(ctx, ethereum.CallMsg{
+		From:     from,
+		To:       &to,
+		Value:    data.Value,
+		GasPrice: gasPrice,
+		Data:     nil,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// functask: cache this
+	chainID, err := c.Client.NetworkID(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	txOps, err := bind.NewKeyedTransactorWithChainID(priK, chainID)
+	if err != nil {
+		return nil, err
+	}
+
+	txOps.Value = data.Value
+	txOps.GasLimit = gasLimit + gasLimit*10/100 // 110% gas limit
+	txOps.GasPrice = gasPrice
+
+	return txOps, nil
+}
